9d9a61292950621ee109fb9bfd552ad8d7c6e193
Arjen <arjen@vansummeren.ch>
2025-12-12 14:49:15 +0100

    Add Nextcloud configuration file and update Docker Compose for custom config

---
+++ docker/nextcloud-config.php
@@ -0,0 +1,17 @@
+
+<?php
+$CONFIG = array (
+  'overwriteprotocol' => 'https',
+  'overwritehost' => 'cloud.vansummeren.ch',
+  'overwritewebroot' => '/',
+  'overwrite.cli.url' => 'https://cloud.vansummeren.ch',
+  'htaccess.RewriteBase' => '/',
+  'trusted_domains' => 
+  array (
+    0 => 'cloud.vansummeren.ch',
+  ),
+  'trusted_proxies' => 
+  array (
+    0 => 'caddy',
+  ),
+);
